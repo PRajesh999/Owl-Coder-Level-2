74550ab57b3baaebae9228ca76ba317beea2efed
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-10-28 21:45:37 +0530

    Add files via upload

---
+++ count 2x2 square.py
@@ -0,0 +1,5 @@
+b=int(input())
+b=b-2
+b=b//2
+print(b*(b+1)//2)
+
---
+++ next palindrome.py
@@ -0,0 +1,8 @@
+n=int(input())
+while(1):
+    n+=1
+    s=str(n)
+    res=s[::-1]
+    if s==res:
+        print(s)
+        break
